4b61cda442c2fdcbec730efed05d55669b1aa565
Carter Jones <carterjones353@u.boisestate.edu>
2025-04-14 10:49:43 -0600

    Update InfoModal.jsx

--- GIMMCheckout/app/components/InfoModal.jsx
+++ GIMMCheckout/app/components/InfoModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Modal, View, Text, Image, Button, StyleSheet } from "react-native";
 import styles from '../app.styles';
+import { API_BASE_URL } from "../config";
 
 export default function InfoModal({ visible, onClose, item}) {
   if (!visible) return null; // Don't render modal if not visible
@@ -15,7 +16,7 @@ export default function InfoModal({ visible, onClose, item}) {
       <View style={styles.modalOverlay}>
         <View style={styles.modalContent}>
         
-          <Image source={{ uri: "http://localhost:5000/local-bucket/" + item.image_url }} style={styles.modalImage} resizeMode="contain"/>
+          <Image source={{ uri: `${API_BASE_URL}/local-bucket/` + item.image_url }} style={styles.modalImage} resizeMode="contain"/>
           <Text style={styles.modalText}>{item.device_name} <b>#{item.device_number}</b></Text> {/* Display the infoText */}
           <Text style={styles.modalText}>{item.description}</Text> {/* Display the decsription */}
           <Text style={styles.modalText}><b>Borrowed:</b> {new Date(item.borrow_datetime).toLocaleDateString()}</Text>
