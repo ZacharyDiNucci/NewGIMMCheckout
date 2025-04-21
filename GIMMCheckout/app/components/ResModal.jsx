3e65f5ff9adf43974d4ba159aa367bc6223bdf86
Carter Jones <carterjones353@u.boisestate.edu>
2025-04-21 13:48:48 -0600

    Update ResModal.jsx

--- GIMMCheckout/app/components/ResModal.jsx
+++ GIMMCheckout/app/components/ResModal.jsx
@@ -6,6 +6,14 @@ import { API_BASE_URL } from "../config";
 export default function InfoModal({ visible, onClose, item}) {
   if (!visible) return null; // Don't render modal if not visible
 
+  const now = new Date();
+  now.setDate(now.getDate() + 7); //TODO: store rental period in db and get here instead of hardcoded
+
+  const mm = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const dd = String(now.getDate()).padStart(2, '0');
+  const yyyy = now.getFullYear();
+
+  const dueDate = `${mm}/${dd}/${yyyy}`;
   return (
     <Modal
       transparent={true}
@@ -19,8 +27,8 @@ export default function InfoModal({ visible, onClose, item}) {
           <Image source={{ uri: `${API_BASE_URL}/local-bucket/` + item.image_url }} style={styles.modalImage} resizeMode="contain"/>
           <Text style={styles.modalText}>{item.device_name} <b>#{item.device_number}</b></Text>
           <Text style={styles.modalText}>{item.description}</Text>
-          <Text style={styles.modalText}><b>Borrowed:</b> {new Date(item.borrow_datetime).toLocaleDateString()}</Text>
-          <Text style={styles.modalText}><b>Due:</b> {new Date(item.due_date).toLocaleDateString()} </Text> 
+          <Text style={styles.modalText}><b>Rental Period:</b> 7d</Text> {/* TODO: dynamically calculate rental period */}
+          <Text style={styles.modalText}><b>Due:</b> {dueDate} </Text> 
           <Button title="Close" onPress={onClose} />
         </View>
       </View>
